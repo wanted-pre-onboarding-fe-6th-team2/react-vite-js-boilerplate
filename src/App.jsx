96a931d5fe2a5eb6c8395c55481cac1ffe04cfef
HaJunRyu <hjr4557@naver.com>
2022-09-02 21:16:28 +0900

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Global } from '@emotion/react';
+import resetCss from '@/styles/reset';
+import { Route, Routes } from 'react-router-dom';
+import Home from '@/pages/Home/Home';
+
+const App = () => (
+  <div>
+    <Global styles={resetCss} />
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </div>
+);
+
+export default App;
